7d6c9f5b4afecb93d573b4c085500df3c5a971e4
denkerrr <tsebin.denis@yandex.ru>
2020-05-22 18:07:45 +0300

    set module

---
+++ modules/munchkin/munchkin-module.js
@@ -0,0 +1,16 @@
+import {MunchkinDesk} from "./munchkin-desk";
+
+export class MunchkinModule {
+
+    desk = new MunchkinDesk();
+    constructor() {
+    }
+
+
+    getHTML() {
+        const html = document.createElement('div');
+        html.id = 'munchkin';
+        html.getElementById('munchkin').innerHTML = this.desk.getHTML()
+        return html;
+    }
+}
